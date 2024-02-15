56233afafc11ae841e7bdb0aa39246402e850e57
LindbergPhilip <143089371+LindbergPhilip@users.noreply.github.com>
2024-02-15 12:36:02 +0100

    Update build.py

--- webserver/build.py
+++ webserver/build.py
@@ -14,7 +14,7 @@ app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
 # change this so that you can connect to your redis server
 # ===============================================
-redis_server = redis.Redis("REDIS_SERVER", decode_responses=True, charset="unicode_escape")
+redis_server = redis.Redis(host = 'localhost', port = '6379', decode_responses=True, charset="unicode_escape")
 # ===============================================
 
 # Translate OSM coordinate (longitude, latitude) to SVG coordinates (x,y).
@@ -42,6 +42,17 @@ def map():
 
 @app.route('/get_drones', methods=['GET'])
 def get_drones():
+    longitude1 = float(redis_server.get("Drone1", 'longitude1'))
+    latitude1 = float(redis_server.get("Drone1", 'latitude1'))
+    status1 = redis_server.get("Drone1", 'status1')
+    longitude1_svg,latitude1_svg = translate((longitude1,latitude1))
+    
+    longitude2 = float(redis_server.get("Drone2", 'longitude2'))
+    latitude2 = float(redis_server.get("Drone2", 'latitude2'))
+    status2 = redis_server.get("Drone2", 'status2')
+    longitude2_svg,latitude2_svg = translate((longitude2,latitude2))
+    
+    
     #=============================================================================================================================================
     # Get the information of all the drones from redis server and update the dictionary `drone_dict' to create the response 
     # drone_dict should have the following format:
@@ -51,7 +62,9 @@ def get_drones():
     #              }
     # use function translate() to covert the coodirnates to svg coordinates
     #=============================================================================================================================================
-    drone_dict = {}
+    drone_dict = {'DRONE_1':{'longitude': longitude1_svg,'latitude': latitude1_svg, 'status': status1},
+                   'DRONE_2': {'longitude': latitude2_svg,'latitude': latitude2_svg, 'status': status2}
+                  }
     return jsonify(drone_dict)
 
 if __name__ == "__main__":
